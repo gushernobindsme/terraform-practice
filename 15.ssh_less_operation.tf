86cbc35439375672946222d6a1b1818d36b09dc3
ERATakumi <eratakumi@gmail.com>
2019-09-21 22:47:34 +0900

    15章分を写経

---
+++ 15.ssh_less_operation.tf
@@ -0,0 +1,85 @@
+# インスタンスプロファイル
+data "aws_iam_policy_document" "ec2_for_ssm" {
+  source_json = data.aws_iam_policy.ec2_for_ssm.policy
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+data "aws_iam_policy" "ec2_for_ssm" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+module "ec2_for_ssm_role" {
+  source     = "./iam_role"
+  name       = "ec2-for-ssm"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ec2_for_ssm.json
+}
+
+resource "aws_iam_instance_profile" "ec2_for_ssm" {
+  name = "ec2-for-ssm"
+  role = module.ec2_for_ssm_role.iam_role_name
+}
+
+# EC2 インスタンス
+resource "aws_instance" "example_for_operation" {
+  ami                  = "ami-Of9ae750e8274075b"
+  instance_type        = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_for_ssm.name
+  subnet_id            = aws_subnet.private_1a.id
+  user_data            = file("./user_data.sh")
+}
+
+output "operation_instance_id" {
+  value = aws_instance.example_for_operation.id
+}
+
+# オペレーションログ
+resource "aws_s3_bucket" "operation" {
+  bucket = "operation-gushernobindsme"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "operation" {
+  name              = "/operation"
+  retention_in_days = 180
+}
+
+resource "aws_ssm_document" "session_manager_run_shell" {
+  name            = "SSM-SessionManagerRunShell"
+  document_type   = "Session"
+  document_format = "JSON"
+
+  content = <<EOF
+  {
+    "schemaVersion": "1.0",
+    "description": "Document to hold regional settings for Session Manager",
+    "sessionType": "Standard_Stream",
+    "inputs": {
+      "s3BucketName": "${aws_s3_bucket.operation.id}",
+      "cloudWatchLogGroupName": "${aws_cloudwatch_log_group.operation.name}"
+    }
+  }
+EOF
+}
